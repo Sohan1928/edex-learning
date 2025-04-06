1a7150e6c9432eb1450be61af15b7315a6a21ae9
Sohan1928 <sohanurrahman1928@gmail.com>
2025-04-06 10:16:21 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/home/navbar/Navbar";
+
+function App() {
+  return (
+    <section>
+      {/* w-[1920px] */}
+      <div className="mx-auto">
+        <Navbar></Navbar>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/home/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="ptFont">
+      <h3>This is navbar components.</h3>
+    </div>
+  );
+};
+
+export default Navbar;
