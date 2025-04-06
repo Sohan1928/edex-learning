5a7359bafed2da24e11914a917d2969e01631cbd
Sohan1928 <sohanurrahman1928@gmail.com>
2025-04-06 17:24:19 +0600

    navbar section start

--- src/components/home/navbar/Navbar.tsx
+++ src/components/home/navbar/Navbar.tsx
@@ -1,8 +1,21 @@
 const Navbar = () => {
   return (
-    <div className="ptFont">
-      <h3>This is navbar components.</h3>
-    </div>
+    <section className="md:px-20 pt-6">
+      <div>
+        <h2 className="italic font-bold text-5xl font-ptFont">
+          <span className="text-[#49bc8a]">e</span>
+          <span className="text-[#ffc278]">Dex</span>
+        </h2>
+        <div className="flex items-center gap-6">
+          <a className="hover:cursor-pointer hover:text-[#56B189]">Home</a>
+          <a>About</a>
+          <a>Course</a>
+          <a>Blog</a>
+          <a>Contact</a>
+        </div>
+        <div></div>
+      </div>
+    </section>
   );
 };
 
