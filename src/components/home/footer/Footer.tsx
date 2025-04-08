fb31d911d5ee91f5257ac2a189bfcc5c90021876
Sohan1928 <sohanurrahman1928@gmail.com>
2025-04-08 12:09:25 +0600

    footer section design are complete

--- src/components/home/footer/Footer.tsx
+++ src/components/home/footer/Footer.tsx
@@ -52,25 +52,36 @@ const Footer = () => {
         </div>
         {/* left 3 */}
         <div>
-          <h3 className="text-xl font-medium mb-5">Explore</h3>
+          <h3 className="text-xl font-medium mb-5 pt-17">Category</h3>
           <span className="flex flex-col space-y-2 opacity-60">
-            <a>Home</a>
-            <a>About</a>
-            <a>Course</a>
-            <a>Blog</a>
-            <a>Contact</a>
+            <a>Design</a>
+            <a>Development</a>
+            <a>Marketing</a>
+            <a>Business</a>
+            <a>Lifestyle</a>
+            <a>Photography</a>
+            <a>Music</a>
           </span>
         </div>
         {/* left 4 */}
         <div>
-          <h3 className="text-xl font-medium mb-5">Explore</h3>
-          <span className="flex flex-col space-y-2 opacity-60">
-            <a>Home</a>
-            <a>About</a>
-            <a>Course</a>
-            <a>Blog</a>
-            <a>Contact</a>
-          </span>
+          <h3 className="text-xl font-medium mb-5 pt-12">Subscribe</h3>
+          <p className="opacity-60">
+            Lorem Ipsum has been them <br /> an industry printer took <br /> a
+            galley make book.
+          </p>
+          <div className="mt-5 bg-[#F3F4F5] rounded-sm">
+            <input
+              className="px-3 py-3"
+              type="email"
+              name="email"
+              id="1"
+              placeholder="Email here"
+            />
+          </div>
+          <button className="bg-[#56B189] mt-4 text-white px-4 py-2 rounded-md hover:cursor-pointer text-[14px] font-normal">
+            Subscribe Now
+          </button>
         </div>
       </div>
     </section>
