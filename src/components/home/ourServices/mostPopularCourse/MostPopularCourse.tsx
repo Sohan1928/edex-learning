dc56d6a856eff58d9cb04cf9bf20132e4951e158
Sohan1928 <sohanurrahman1928@gmail.com>
2025-04-12 12:19:05 +0600

    course cart gsap animation updated

--- src/components/home/ourServices/mostPopularCourse/MostPopularCourse.tsx
+++ src/components/home/ourServices/mostPopularCourse/MostPopularCourse.tsx
@@ -13,7 +13,7 @@ const MostPopularCourse = () => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: "#mostPopularCourseSection",
-        start: "top 60%",
+        start: "top 40%",
         end: "top 20%",
         toggleActions: "play none none reverse", // Optional for smoother scroll back
         markers: true,
@@ -24,8 +24,22 @@ const MostPopularCourse = () => {
       x: -100,
       opacity: 0,
       scale: 0.95,
-      duration: 1.6,
+      duration: 1.2,
       ease: "power3.out",
+      stagger: 0.4,
+    });
+    gsap.from("#slideCart", {
+      x: 250,
+      opacity: 0,
+      duration: 1.2,
+      scale: 0.4,
+      ease: "power1.out",
+      scrollTrigger: {
+        trigger: "#mostPopularCourseSection",
+        start: "top 40%",
+        end: "top 20%",
+        toggleActions: "play none none reverse",
+      },
     });
   });
 
@@ -35,16 +49,19 @@ const MostPopularCourse = () => {
       className="md:px-28 mt-28 bg-[#ecfff8]"
     >
       <div className="py-10 flex justify-between items-center">
-        <div id="mostPopularText" className="relative">
-          <h2 className="text-5xl leading-14 font-semibold">
+        <div className="relative">
+          <h2
+            id="mostPopularText"
+            className="text-5xl leading-14 font-semibold"
+          >
             Most <br /> <span className="text-[#49bc8a]">Popular</span> <br />{" "}
             <span className="text-[#49bc8a]">Course</span>
           </h2>
-          <div className="absolute top-26.5">
+          <div id="mostPopularText" className="absolute top-26.5">
             <YellowLineSvg></YellowLineSvg>
           </div>
         </div>
-        <div>
+        <div id="slideCart">
           <SliderCart></SliderCart>
         </div>
       </div>
